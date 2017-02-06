68c9d3722756d50b9c114e4657d03672e2762c2d
Sukora-Stas <stasiksukora@mail.ru>
2017-02-06 10:40:47 +0300

    CountryH

---
+++ ClassWork/JavaScript/тема2/CountryH.js
@@ -0,0 +1,33 @@
+/**
+ * Created by stasi on 06.02.2017.
+ */
+var CountrysH = {};
+
+function AddCountry(CountryName, CapitalName) {
+    CountrysH[CountryName] = CapitalName;
+}
+
+function DeleteCountry(CountryName) {
+    delete CountrysH[CountryName];
+}
+
+function GetCountryInfo(CountryName) {
+    if (CountryName in CountrysH)
+        return 'страна: ' + CountryName + ' столица: ' + CountrysH[CountryName];
+    else
+        return 'нет информации о стране ' + CountryName + '!';
+}
+
+function ListCountrys() {
+    var Res = "";
+    for (var CN in CountrysH)
+        Res += '\n' + GetCountryInfo(CN);
+    return Res;
+}
+
+AddCountry('Германия', 'Берлин');
+AddCountry('Венгрия', 'Будапешт');
+AddCountry('Франция', 'Париж');
+
+console.log("список стран:" + ListCountrys());
+
