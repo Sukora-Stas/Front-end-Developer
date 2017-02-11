f90b7644c2cf83b11ba8c38dfed89f49be8a641a
Sukora-Stas <stasiksukora@mail.ru>
2017-02-11 15:44:34 +0300

    project

--- Project/keksbi/js/script.js
+++ Project/keksbi/js/script.js
@@ -14,5 +14,27 @@ $(function () {
         }
     );
     /////////////////////////
+    // var site = 'order.html';
+    //
+    // $('#test').click(function () {
+    //
+    //         //noinspection JSValidateTypes
+    //         if(location.href==('order.html').statusCode){
+    //
+    //             location.href = 'order.html'
+    //
+    //     }
+    //     else
+    //         location.href = 'EROR404/eror404.html';
+    //
+    // });
+
+
+    $('.header-logo').click(function () {
+        location.href = 'index.html';
+    });
+
 
 });
+
+// window.location = "EROR404/eror404.html";
