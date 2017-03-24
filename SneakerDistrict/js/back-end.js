674b9528c6fb106ef5801edd19fcb7fe31798225
Sukora-Stas <stasiksukora@mail.ru>
2017-03-24 19:54:03 +0300

    SneakerDistrict

--- SneakerDistrict/js/back-end.js
+++ SneakerDistrict/js/back-end.js
@@ -6,11 +6,21 @@ $(document).ready(function () {
     $('.catalog-item:hover .none').css('display', 'block');
 }
     );
+
     $('.catalog-item').mouseleave(
         function () {
             $('.none').css('display', 'none');
             $('.block').css('display', 'block');
         }
     );
+
+
+    (function(e){
+        jQuery(document).textmistake({
+            'mailTo': 'andreysx(d0g)@tut.by'.replace(/\(d0g\)/, '@'),
+            'mailFrom': 'st.sukora(d0g)@gmail.com'.replace(/\(d0g\)/, '@'),
+            'mandrillKey': 'f8288a5316e674345e419b0f6015faa2-us15', // Get your - https://mandrill.com/signup/
+        });
+    })(jQuery)
 
 });
