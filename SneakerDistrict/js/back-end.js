eb78a0f4c496d13da9332a1c45a3831566913db7
Sukora-Stas <stasiksukora@mail.ru>
2017-03-24 15:11:12 +0300

    SneakerDistrict

--- SneakerDistrict/js/back-end.js
+++ SneakerDistrict/js/back-end.js
@@ -1,15 +1,6 @@
 $(document).ready(function () {
 
 
-
-    // $('.catalog-item').not().hover(
-    //
-    //     function () {
-    //         $('.block').removeClass('none');
-    //         $('.none:hover').removeClass('block');
-    //     }
-    // );
-
     $('.catalog-item').mouseenter(
         function () {
     $('.catalog-item:hover .block').css('display', 'none');
@@ -22,17 +13,6 @@ $(document).ready(function () {
             $('.block').css('display', 'block');
         }
     );
-
-    // $('.catalog-item').hover(
-    //     function () {
-    //         $('.block').css('display', 'none');
-    //         $('.none').css('display', 'block');
-    //     },
-    //     function () {
-    //         $('.none').css('display', 'none');
-    //         $('.block').css('display', 'block');
-    //     }
-    // );
 
 
 });
